47adca150ead69cb6dbc4e1a00958f9b08abe4d2
Jai <jaisankar.n@gmail.com>
2013-09-23 15:24:45 +0530

    Nagarajan Family

---
+++ classes/durga.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+</head>
+<?php
+print_r($_POST);
+?>
+<body style="text-align:center;">
+  <div class="header"><div> <a href="index.html">
+  
+	      <button type="home.jpg" value="home"> home</button></a></div>
+  </div>
+  
+  <div class="content">
+		  <img src="images/durga.JPG" alt="durga" width="200" height="150">
+
+  <p>This my mother-in- law and sister-in -law</br>
+     They are pray to God . she is newly married and come to our house first time.</p>
+</div>
+<div>
+     <ul>
+         <li> Nagarajan</li> 
+		 <li> skandan</li> 
+		 <li> jaisankar</li> 
+		 <li> mathangi</li> 
+		 <li> anand</li> 
+      </ul>
+</div>
+<form method="post">
+ First name:<input type="text" name ="Firstname" value="name"><br>
+ Last name:<input type="text" name="Lastname" value="name"><br>
+ sex:      <input type="text"name="sex" value="sex"><br>
+ cell no:  <input type="text"name="cellno" value="cellno"><br>
+ Mail ID:  <input type="text"name="mailId" value="MailId"><br>
+ <input type="submit" value="submit">
+ </form>
+
+<footer >
+<p>
+        <a href="nagarajan.html">nagarajan</a>|
+        <a href="jaisankar.html">jaisankar</a>|
+        <a href="mathangi.html">mathangi</a>|
+		<a href="skandan.html">skandan</a>|
+		<a href="sub/anand.html">anand</a>|
+</p>
+	
+</footer>
+</body>
+</html>
+
